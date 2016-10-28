6bba2a9a7362b8412b87180217faf4416c168459
Grzesiek Pluta <pluta7@gmail.com>
2016-10-28 22:00:59 +0200

    Remove MongoDB repo

--- vagrant_bootstrap/bootstrap.sh
+++ vagrant_bootstrap/bootstrap.sh
@@ -1,7 +1,4 @@
 #@IgnoreInspection BashAddShebang
-# Add MongoDB repository
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 #update apt
 sudo apt-get update
 
